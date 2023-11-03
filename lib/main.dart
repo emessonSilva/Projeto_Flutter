a84f9022c6361b035051febcdefdbdd1f187208b
Emesson <emessonbarbosa600@gmail.com>
2023-11-02 22:15:20 -0300

    Criação da tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,13 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color buttonColor = const Color(0xFF5E548E);
+    void navigateToRegisterScreen() {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const RegisterScreen(),
+        ),
+      );
+    }
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -137,7 +143,7 @@ class LoginScreen extends StatelessWidget {
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: buttonColor,
+                  backgroundColor: Color(0xFF5E548E),
                   minimumSize: const Size(250, 50),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
@@ -152,16 +158,19 @@ class LoginScreen extends StatelessWidget {
 
             //Texto de Ainda não tem cadastro? Registre-se
             Container(
-              margin: const EdgeInsets.only(
-                top: 30,
-              ),
-              child: const Text(
-                'Ainda não tem cadastro? Registre-se',
-                style: TextStyle(
+              margin: const EdgeInsets.only(top: 30),
+              child: InkWell(
+                onTap:
+                    navigateToRegisterScreen, // Navegar para a tela de cadastro
+                child: const Text(
+                  'Ainda não tem cadastro? Registre-se',
+                  style: TextStyle(
                     fontSize: 14.5,
                     color: Color(0xFF646464),
                     fontWeight: FontWeight.w500,
-                    fontFamily: 'Poppins'),
+                    fontFamily: 'Poppins',
+                  ),
+                ),
               ),
             )
           ],
@@ -170,3 +179,185 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Tela de cadastro
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key, Key? keys});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //AppBar para colocar a seta
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop(); // Voltar para a tela anterior
+          },
+          color: Color(0xFF5E548E),
+          iconSize: 30,
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Texto "Cadastre-se"
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 20),
+                child: const Text(
+                  'Cadastre-se',
+                  style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Poppins'),
+                ),
+              ),
+
+              // Texto Nome de Usuário
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Nome de Usuário',
+                      prefixIcon: Icon(
+                        Icons.person_outline,
+                        color: Color(0xFF9F86C0),
+                      ),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //Texto Número de telefone
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Número de telefone',
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Color(0xFF9F86C0),
+                      ),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //Texto Email
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Icon(
+                        Icons.alternate_email_rounded,
+                        color: Color(0xFF9F86C0),
+                      ),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //Texto Senha
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      prefixIcon: Icon(
+                        Icons.lock_rounded,
+                        color: Color(0xFF9F86C0),
+                      ),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //Botão Cadastrar
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 50,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF5E548E),
+                    minimumSize: const Size(250, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cadastrar',
+                    style: TextStyle(fontSize: 18, fontFamily: 'Poppins'),
+                  ),
+                ),
+              ),
+
+              //Texto Já possui uma conta? Entre
+              Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop(); // Retorna à tela anterior
+                  },
+                  child: const Text(
+                    'Já possui uma conta? Entre',
+                    style: TextStyle(
+                      fontSize: 14.5,
+                      color: Color(0xFF646464),
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Tela de recuperar a senha
