c06e0b0081b0d2e41d45f8f6f2c01763381d7d70
Emesson <emessonbarbosa600@gmail.com>
2023-11-15 22:33:29 -0300

    tela de generos

--- lib/main.dart
+++ lib/main.dart
@@ -154,7 +154,7 @@ class LoginScreen extends StatelessWidget {
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF5E548E),
+                  backgroundColor: const Color(0xFF5E548E),
                   minimumSize: const Size(250, 50),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
@@ -195,41 +195,41 @@ class LoginScreen extends StatelessWidget {
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key, Key? keys});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //AppBar para colocar a seta
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop(); // Voltar para a tela anterior
-          },
-          color: Color(0xFF5E548E),
-          iconSize: 30,
-        ),
-      ),
       body: SingleChildScrollView(
         reverse: true,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  right: 320,
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Voltar para a tela anterior
+                  },
+                  color: const Color(0xFF5E548E),
+                  iconSize: 30,
+                ),
+              ),
+
               // Texto "Cadastre-se"
               Container(
                 margin: const EdgeInsets.only(top: 10, bottom: 20),
                 child: const Text(
                   'Cadastre-se',
                   style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'Poppins'),
+                    fontSize: 32,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Poppins',
+                  ),
                 ),
               ),
 
@@ -326,9 +326,15 @@ class RegisterScreen extends StatelessWidget {
                   top: 50,
                 ),
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => GenreScreen(),
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF5E548E),
+                    backgroundColor: const Color(0xFF5E548E),
                     minimumSize: const Size(250, 50),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
@@ -377,20 +383,202 @@ class PasswordRecoveryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //AppBar para colocar a seta
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
+        //AppBar para colocar a seta
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop(); // Voltar para a tela anterior
+            },
+            color: const Color(0xFF5E548E),
+            iconSize: 30,
           ),
-          onPressed: () {
-            Navigator.of(context).pop(); // Voltar para a tela anterior
-          },
-          color: Color(0xFF5E548E),
-          iconSize: 30,
         ),
+        body: SingleChildScrollView(
+            reverse: true,
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  // Texto "Cadastre-se"
+                  Container(
+                    margin: const EdgeInsets.only(top: 10, bottom: 20),
+                    child: const Text(
+                      'Recuperar senha',
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Poppins'),
+                    ),
+                  ),
+                ]))));
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//Tela de escolha de gêneros
+class GenreScreen extends StatefulWidget {
+  const GenreScreen({Key? key}) : super(key: key);
+
+  @override
+  _GenreScreenState createState() => _GenreScreenState();
+}
+
+class _GenreScreenState extends State<GenreScreen> {
+  final List<bool> _isSelected = List.generate(12, (_) => false);
+  final List<String> genres = [
+    'Suspense',
+    'Ação',
+    'Comédia',
+    'Romance',
+    'Drama',
+    'Ficção Científica',
+    'Animação',
+    'Fantasia',
+    'Documentário',
+    'Mistério',
+    'Aventura',
+    'Thriller',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    int halfLength = (genres.length / 2).ceil();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 8,
+              left: 8,
+              right: 50,
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Voltar para a tela anterior
+                  },
+                  color: const Color(0xFF5E548E),
+                  iconSize: 30,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 14),
+                        width: 34,
+                        height: 34,
+                        child: Image.asset('assets/images/img_claquete.png'),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 6),
+                        child: const Text(
+                          'ClackMovie',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(45),
+            child: Text(
+              'Quais tipos de filmes você gosta?',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: halfLength,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: _isSelected[index]
+                                ? Colors.amber
+                                : Colors.black26,
+                          ),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _isSelected[index] = !_isSelected[index];
+                            });
+                          },
+                          child: Card(
+                            child: Row(
+                              children: [
+                                Icon(Icons.movie),
+                                SizedBox(width: 16),
+                                Text(genres[index]),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: halfLength,
+                    itemBuilder: (context, index) {
+                      int newIndex = index + halfLength;
+                      return Container(
+                        margin: const EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: _isSelected[newIndex]
+                                ? Colors.amber
+                                : Colors.black26,
+                          ),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              _isSelected[newIndex] = !_isSelected[newIndex];
+                            });
+                          },
+                          child: Card(
+                            child: Row(
+                              children: [
+                                Icon(Icons.movie),
+                                SizedBox(width: 16),
+                                Text(genres[newIndex]),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
